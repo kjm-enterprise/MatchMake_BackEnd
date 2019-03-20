33a882913fd8ef85ab7a50ddbed444dde712d6bc
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-03-20 17:01:09 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/kjmenterprise/matchmaker/MatchMakerApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.kjmenterprise.matchmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMakerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MatchMakerApplication.class, args);
+  }
+
+}
